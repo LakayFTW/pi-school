bcba1a850ca3ef1085fe20a606a48d37b810751a
LakayFTW <MilchmannLetsPlay@gmail.com>
2022-06-30 16:36:13 +0200

    created method to write results from db into json file

---
+++ js/jsonExtractor.js
@@ -0,0 +1,10 @@
+const tempJson = '../json/temperatures.json';
+const weatherJson = '../json/testdata.json';
+
+function getDBEntriesFromJson() {
+
+}
+
+function getWeatherFromAPI() {
+    
+}
--- script/dbfetch.py
+++ script/dbfetch.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from datetime import datetime
+import json
+import os
 
 connect = mysql.connector.connect(
     host="localhost",
@@ -14,6 +16,27 @@ cursor.execute("SELECT days.datetime, temperatures.temp FROM days INNER JOIN tem
 
 result = cursor.fetchall()
 print("Row Count: ", len(result))
+
+json_obj = {}
+json_obj['temperatures'] = []
+data = []
+
+counter = 0;
 for x in result:
-    print(x[0])
-    print(x[1])
+    date = str(x[0])
+    temp = str(x[1])
+
+    item = {}
+    item[counter] ={'date': date, 'temp': temp}
+    data.append(item)
+
+    counter += 1
+    
+
+print(data)
+
+if (os.path.exists('json/temperatures.json')):
+    os.remove('json/temperatures.json')
+
+with open('json/temperatures.json', 'w') as jsonfile:
+    json.dump(data, jsonfile)
