3c541c1f775cc6f4eeef4617f30a4e0898e7efac
LakayFTW <MilchmannLetsPlay@gmail.com>
2022-07-04 00:58:55 +0200

    added functions to read from local josn

--- js/jsonExtractor.js
+++ js/jsonExtractor.js
@@ -2,9 +2,27 @@ const tempJson = '../json/temperatures.json';
 const weatherJson = '../json/testdata.json';
 
 function getDBEntriesFromJson() {
-
+    fetch("./json/temperatures.json")
+    .then(response => {
+    return response.json();
+    })
+    .then(jsondata => {
+        var tempjson = jsondata;
+        console.log(tempjson);
+    });
 }
 
 function getWeatherFromAPI() {
+    fetch("./json/testdata.json")
+    .then(response => {
+    return response.json();
+    })
+    .then(jsondata => {
+        var json = jsondata;
+        pasteResultToHtml(json);
+    });
+}
+
+function pasteResultToHtml(json){
     
 }
